80d66fa9a25901b6bf729bc59995140830841e1c
chittapu <chittapu.rug@awecent.com>
2016-06-20 17:58:00 +0700

    fontawesome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <title>Insurance</title>
     <link rel="stylesheet" type="text/css" href="{{ asset("components/css/reset.css") }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset("components/font-awesome/css/font-awesome.min.css") }}">
     <link rel="stylesheet" type="text/css" href="{{ asset("components/bootstrap/dist/css/bootstrap.min.css") }}">
     <link rel="stylesheet" type="text/css" href="{{ asset("components/bootstrap/dist/css/bootstrap-theme.min.css") }}">
 
@@ -19,6 +20,89 @@
 <body>
 <div class="wrapper">
     <div class="parallax">
+
+        <header>
+            <div class="top-informations-bg">
+
+                <div class="container">
+
+                    <div class="row">
+
+                        <div class="col-xs-12 col-sm-12">
+                            <div class="top-informations">
+                                <ul>
+
+                                    <li class="phone-number">
+                                        <i class="fa fa-phone"></i>
+                                        <a href="tel:020 1234 678">
+                                            020 1234 678
+                                        </a>
+                                    </li>
+                                    <li class="e-mail">
+                                        <i class="fa fa-envelope-o"></i>
+                                        <a href="mailto:info@example.com">
+                                            info@example.com
+                                        </a>
+                                    </li>
+                                    <li class="e-skype">
+                                        <i class="fa fa-skype"></i>
+                                        <a href="skype:example.skype?call">
+                                            example.skype
+                                        </a>
+                                    </li>
+
+                                </ul>
+                            </div>
+
+
+                            <div class="connect-with-us">
+
+                                <ul>
+
+
+                                    <li>Connect With Us:</li>
+
+
+                                    <li class="social-icons">
+
+
+
+                                        <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+
+
+                                        <a href="#" target="_blank"><i class="fa fa-facebook"></i> </a>
+
+
+                                        <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+
+
+                                        <a href="#" target="_blank"><i class="fa fa-instagram"></i></a>
+
+
+                                        <a href="#" target="_blank"><i class="fa fa-google-plus"></i></a>
+
+
+                                        <a href="#" target="_blank"><i class="fa fa-pinterest"></i></a>
+
+
+                                    </li>
+
+                                </ul>
+
+                            </div> <!-- col-xs-12 col-sm-6 connect-with-us -->
+
+
+
+
+                        </div> <!-- row -->
+
+                    </div> <!-- container -->
+
+                </div><!-- top-informations-bg -->
+            </div><!-- top-informations-bg -->
+        </header>
+
+
         <input type="checkbox" id="menu-toggle" class="hidden">
         <label for="menu-toggle">
             <span class="glyphicon"></span>
@@ -42,6 +126,8 @@
                 <li><a href="javascript:void(0)">Messages</a></li>
             </ul>
         </div><!--/.menu-->
+
+
         <div class="content">
             <div class="container">
                 <div class="row">
@@ -65,9 +151,10 @@
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras suscipit id orci eget vulputate. Etiam in libero sed magna fermentum condimentum.</p>
                     </div><!--/.col-sm-4-->
                 </div><!--/.row-->
-
             </div><!--/.container-->
         </div><!--/.content-->
+
+
     </div><!--/.parallax-->
 </div>
 </body>
