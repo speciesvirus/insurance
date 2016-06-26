651010e8d522b2e96a449cc50c2ab43e89fbe78d
Plai <chittapuu@gmail.com>
2016-06-26 22:35:04 +0700

    update all

--- resources/views/ecir.blade.php
+++ resources/views/ecir.blade.php
@@ -9,7 +9,7 @@
     <meta property="og:type"          content="website" />
     <meta property="og:title"         content="สัญญาเพิ่มเติมกลุ่มโรคร้ายแรง" />
     <meta property="og:description"   content="เมื่อตรวจพบว่าเป็นโรคร้ายแรงเป็นครั้งแรกในขณะที่ยังมีชีวิตอยู่ การได้รับชดเชยเงินก้อนจะช่วยในการวางแผนการรักษาได้เป็นอย่างมาก อย่าปล่อยให้โรคร้ายแรงทำร้ายคนทั้งบ้าน ให้เอไอเอ เป็นผู้ดูแลคุณและคนที่คุณรัก" />
-    <meta property="og:image"         content="{{ asset("components/image/ECIR/Manual_Rider-01.png") }}" />
+    <meta property="og:image"         content="{{ asset("components/image/ECIR/ecir_logo.jpg") }}" />
 @stop
 
 @section('content')
